0947fa80ba1a14afb99dd6be9851d2528a047b74
Md. Monoarul Islam Moon <moon.work247@gmail.com>
2025-09-09 23:19:44 +0600

    nginx is better

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -402,7 +402,7 @@ export default function AdminPage() {
                           <div className="text-sm font-semibold text-gray-900 mb-1">
                             {link.itineraryUrl && (
                               <ItineraryCell
-                                itineraryUrl={"/api/" + link.itineraryUrl}
+                                itineraryUrl={link.itineraryUrl}
                               />
                             )}
                           </div>
